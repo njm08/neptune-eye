273a352c12bb36e5696b48dadc667f029ca78bc3
Niklas Meier <njm08@yahoo.com>
2025-11-13 12:24:09 +0100

    More docker stuff :)

---
+++ tools/docker/build_docker_training_amd64.py
@@ -0,0 +1,16 @@
+"""
+Build Docker image for training.
+"""
+
+from build_docker_util import build_docker
+
+IMAGE_NAME: str = 'njm08/neptune-eye'
+TAG: str = 'latest-training-amd64'
+DOCKER_FILE: str = 'Dockerfile.training-amd64'
+
+if __name__ == "__main__":
+        build_docker(
+        dockerfile=DOCKER_FILE,
+        dockerimage=IMAGE_NAME,
+        tag=TAG,
+        platforms=["linux/amd64"])
